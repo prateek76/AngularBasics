2af113c5f70dfdd83ff6a2e7156a75b3a93def03
Prateek76 <iec2017029@iiita.ac.in>
2019-11-11 23:21:08 +0530

    first commit

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'event-thumbnail',
+    template: `
+    <div class="well">
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+    </div>
+    `,
+    styles: [`
+        .pad-left {margin-left: 10px;}
+        .well {color: #222}
+        `
+    ]
+})
+
+export class EventThumbnailComponent{
+    @Input() event:any
+
+    /*someProperty:any = "some value"
+
+    logfoo() {
+        console.log("fooChk");
+    }*/
+}

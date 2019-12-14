2d4693cd4a58f42dfd54f7efaf5b66700e25896a
Prateek76 <iec2017029@iiita.ac.in>
2019-12-14 16:15:24 +0530

    exported all imported one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { EventRouteActicator } from './events/event-details/event-route-activator.service';
 import { Error404Component } from './errors/404.component';
-import { CreateEventComponent } from './events/create-event.component';
 import { ToastrService } from './common/toastr.service';
-import { EventService } from './events/shared/event.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  EventRouteActicator,
+  CreateEventComponent,
+  EventService
+} from './events/index';
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
 import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 @NgModule({
   declarations: [

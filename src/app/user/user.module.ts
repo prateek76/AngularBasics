0dbd15a679774f1ebcc9c6aaeebe5f218d7e6765
Prateek76 <iec2017029@iiita.ac.in>
2019-12-17 03:21:02 +0530

    reactive forms

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,11 +4,12 @@ import { ProfileComponent } from './profile.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         FormsModule,
+        ReactiveFormsModule,
         CommonModule,
         RouterModule.forChild(userRoutes)
     ],
